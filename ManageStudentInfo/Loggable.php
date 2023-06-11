d9f7f8e469b9177654ad6a88628ec6c09f0b1c3f
Maysa'a Sa'ad <maysaa.saad118@gmail.com>
2023-06-11 13:17:23 +0300

    arrays

--- ManageStudentInfo/Loggable.php
+++ ManageStudentInfo/Loggable.php
@@ -1,11 +1,9 @@
 <?php
 
-namespace ManageStudentInfo ;
-
-use  Student;
-////use  ManageStudentInfo\Student;
 
 trait Loggable {
+  const filename = "log.txt";
+  
     public function log(string $message) {
       $logFile = fopen('log.txt','a');
       fwrite($logFile , $message ."\n");
--- ManageStudentInfo/Student.php
+++ ManageStudentInfo/Student.php
@@ -1,4 +1,5 @@
 <?php
+namespace MANAGESTUDENT;
 
 class Student {
 
